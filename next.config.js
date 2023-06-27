764762e8ec53d06892d206bfa388b8d0180111b6
LferrazCoelho <lferraz.dev@gmail.com>
2023-06-26 23:55:37 -0300

    Play Masters

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
+  images: {
+    domains: ['www.freetogame.com'],
+  },
 }
 
 module.exports = nextConfig
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,124 +1,150 @@
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import Link from 'next/link';
+import { useState, useEffect } from "react"
 
-const inter = Inter({ subsets: ['latin'] })
+const URL_API = "https://games-test-api-81e9fb0d564a.herokuapp.com/api/data/"
 
 export default function Home() {
+
+  const [loading, setLoading] = useState(false);
+  const [statusApi, setStatusApi] = useState(0);
+  const [listGames, setListGames] = useState(null);
+  const [endTime, setEndTime] = useState(false);
+  const [msgPage, setMsgPage] = useState(false);
+
+  const controller = new AbortController()
+  const signal = controller.signal
+
+  const fetchData = async () => {
+    try {
+      setLoading(true)
+
+      setTimeout(stopWatch, 5000)
+
+      const response = await fetch(URL_API, {
+        method: 'GET',
+        signal: signal,
+        headers: {
+          "dev-email-address": "lferraz.dev@gmail.com"
+        },
+      })
+
+      const data = await response.json()
+
+      setStatusApi(response.status)
+      setListGames(data)
+
+    } catch (error) {
+      console.log(error)
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  function stopWatch() {
+    setEndTime(true)
+    controller.abort()
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
+    <main className="flex justify-center items-center w-screen min-h-screen p-10 md:p-24">
+      <div>
+        { loading && !listGames &&
+          <div className="flex flex-col justify-center items-center">
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
+              className=""
+              src="/loading.svg"
+              alt="Carregando a lisat de jogos"
               width={100}
-              height={24}
+              height={100}
               priority
             />
-          </a>
-        </div>
+            <p className="animate-pulse">Loading Games list</p>
+          </div>
+        }
+
+        { endTime && !listGames &&
+          <div className="flex flex-col justify-center items-center">
+            <Image
+              className="m-8"
+              src="/endtime.png"
+              alt="Carregando a lisat de jogos"
+              width={80}
+              height={80}
+              priority
+            />
+            <p className="w-80 text-center">The server took a while to respond, please try again later.</p>
+            <button className="bg-gray-700 m-8 py-2 px-3 rounded-lg hover:bg-gray-800" onClick={() => window.location.reload()}>refresh page</button>
+          </div>
+        }
+
+        { statusApi >= 500 &&
+          <div className="flex flex-col justify-center items-center">
+            <Image
+              className="m-8"
+              src="/apifail.png"
+              alt="Carregando a lisat de jogos"
+              width={80}
+              height={80}
+              priority
+            />
+            <p className="w-80 text-center">The server failed to respond, try reloading the page.</p>
+            <button className="bg-gray-700 m-8 py-2 px-3 rounded-lg hover:bg-gray-900" onClick={() => window.location.reload()}>reload page</button>
+          </div>
+        }
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-3">
+        {statusApi == 200 && listGames.map((item, key) =>
+            <div className="bg-gray-700 rounded-lg m-4 text-gray-200" key={key}>
+              <Image
+                className="rounded-t-lg w-full object-cover object-center"
+                src={item.thumbnail}
+                alt="Carregando a lisat de jogos"
+                width={100}
+                height={100}
+                priority
+              />
+              <div className="p-4">
+                <p className="mb-2 font-bold">
+                  {item.title}
+                </p>
+                <p className="text-sm">
+                  Genre: {item.genre}
+                </p>
+                <p className="text-sm">
+                  Platform: {item.platform}
+                </p>
+                <p className="text-sm mb-2">
+                  Publisher: {item.publisher}
+                </p>
+                <Link 
+                  className="w-full text-gray-800 bg-gray-200 my-4 py-2 px-3 rounded-lg hover:bg-gray-400"
+                  href={item.freetogame_profile_url}
+                  >
+                  to play
+                </Link>
+              </div>
+            </div>
+        )}
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
   )
 }
+
+        // "id": 521,
+        // "title": "Diablo Immortal",
+        // "thumbnail": "https://www.freetogame.com/g/521/thumbnail.jpg",
+        // "short_description": "Built for mobile and also released on PC, Diablo Immortal fills in the gaps between Diablo II and III in an MMOARPG environment.",
+        // "game_url": "https://www.freetogame.com/open/diablo-immortal",
+        // "genre": "MMOARPG",
+        // "platform": "PC (Windows)",
+        // "publisher": "Blizzard Entertainment",
+        // "developer": "Blizzard Entertainment",
+        // "release_date": "2022-06-02",
+        // "freetogame_profile_url": "https://www.freetogame.com/diablo-immortal"    
